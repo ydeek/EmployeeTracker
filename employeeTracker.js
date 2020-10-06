ac712c3ba14224d97808c8dc700f6f57977b4c66
Yazan Deek <ydeek91@yahoo.com>
2020-10-06 17:10:07 -0400

    declared dependencies.

---
+++ employeeTracker.js
@@ -0,0 +1,11 @@
+const inquirer = require("inquirer");
+let Database = require("./db");
+let cTable = require("console.table");
+
+const db = new Database({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Shifting Shadows",
+    database: "cms"
+});
