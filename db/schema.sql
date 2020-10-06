4983226c6b3cde6b7de4c171ef898f02edfda8e5
Yazan Deek <ydeek91@yahoo.com>
2020-10-06 17:31:26 -0400

    updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,34 @@
+DROP DATABASE IF EXISTS employees_DB;
 
-DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE DATABASE employees_DB;
 
-CREATE database employee_trackerDB;
+USE employees_DB;
 
-USE employee_trackerDB;
-
+----- Create Department table -----
 CREATE TABLE department (
-  id INTEGER NOT NULL auto_increment PRIMARY KEY,
-  name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
+----- Create Role Table -----
 CREATE TABLE role (
-  id INTEGER NOT NULL auto_increment PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INTEGER,
-  FOREIGN KEY (department_id) REFERENCES department(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
+----- Create Employee Table -----
 CREATE TABLE employee (
-  id INTEGER NOT NULL auto_increment PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INTEGER,
-  manager_id INTEGER,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES role(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
