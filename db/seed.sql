e3fb3beb13112fab87aeb68feeb53a0ef4433427
Yazan Deek <ydeek91@yahoo.com>
2020-10-06 08:07:40 -0400

    pre-populated data base for department in seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+USE employee_trackerDB;
+
+INSERT into department (name) VALUES ("Sales");
+INSERT into department (name) VALUES ("IT");
+INSERT into department (name) VALUES ("Bob");
+INSERT into department (name) VALUES ("HR");
